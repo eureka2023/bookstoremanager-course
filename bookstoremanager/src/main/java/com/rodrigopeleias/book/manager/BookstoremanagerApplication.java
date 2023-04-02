cf35bc3e2ab39d3bdfe7cc749d45f4e34d8b31c8
eureka2023 <eastiservice@gmail.com>
2023-04-01 23:48:27 -0300

    First Commit em 01.04.2023

---
+++ bookstoremanager/src/main/java/com/rodrigopeleias/book/manager/BookstoremanagerApplication.java
@@ -0,0 +1,21 @@
+/*
+Projeto BookStoreManager
+Livraria Virtual
+Rodrigo Peleias
+Em 01.04.2023
+Euripedes Silva
+ */
+
+package com.rodrigopeleias.book.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoremanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoremanagerApplication.class, args);
+	}
+
+}
